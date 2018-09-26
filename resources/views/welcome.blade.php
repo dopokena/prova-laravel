dd5bf2054a86f42f4eaa6a12f012197f88740066
dopokena <acarreras.97@gmail.com>
2018-09-26 12:05:46 +0200

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel prova Blog
+                    a ver si va
                 </div>
 
                 <div class="links">
